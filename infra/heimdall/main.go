13c01d9acc92f4cc231a77db65f2711b6a758e0e
Jens Skott <jens.skott@cathex.io>
2023-02-18 08:20:26 +0000

    initial commit

--- infra/heimdall/main.go
+++ infra/heimdall/main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"github.com/pulumi/pulumi-gcp/sdk/v6/go/gcp/organizations"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	"os"
 )
 
 func main() {
+	projectId := os.Getenv("GOOGLE_PROJECT")
+
 	pulumi.Run(func(ctx *pulumi.Context) error {
+		project, err := organizations.LookupProject(ctx, &organizations.LookupProjectArgs{ProjectId: &projectId})
+		if err != nil {
+			return err
+		}
 		return nil
 	})
 }
